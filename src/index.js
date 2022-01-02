7e51fa95f727b2d238b6ffbc899aa20c6a8618e5
nill <naadir.iqbal1@gmail.com>
2022-01-01 23:48:16 -0600

    moved form submit into own module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import { events } from './events.js';
-import { toDo } from './toDo.js';
-import { createToDoDOM } from './createToDoDOM';
+import { submitToDoForm } from './submitToDoForm.js';
+// import { toDo } from './toDo.js';
+// import { createToDoDOM } from './createToDoDOM';
+// import { formSubmit } from './formSubmit.js'
 
 let projects = { 'home': [] };
 let currentProject = 'home';
+localStorage.setItem('currentProject', currentProject);
+localStorage.setItem('projects', JSON.stringify(projects));
+
 events();
-
-window.formSubmit = function formSubmit() {
-    const title = document.querySelector('#form-title').value;
-    const description = document.querySelector('#form-description').value;
-    const date = document.querySelector('#form-date').value;
-    const priority = document.querySelector('input[name="form-priority"]:checked').value;
-    document.querySelector('#to-do-form').reset();
-    document.querySelector('#to-do-form').style.display = 'none';
-
-    let aToDo = toDo(title, description, date, priority);
-    createToDoDOM(aToDo, currentProject);
-    projects[currentProject].push(aToDo);
-}
+submitToDoForm();
 
 window.projectFormSubmit = function projectFormSubmit() {
     let newProjTitle = document.querySelector('#project-title').value.toLowerCase();
@@ -46,9 +39,7 @@ window.projectFormSubmit = function projectFormSubmit() {
     document.querySelector('#new-project-form').reset();
     document.querySelector('#new-project-form').style.display = 'none';
 }
-
 document.querySelector('#new-project-form').setAttribute('onsubmit', 'projectFormSubmit();return false');
-document.querySelector('#to-do-form').setAttribute('onsubmit', 'formSubmit();return false');
 
 document.querySelector('#home').addEventListener('click', () => {
     document.querySelectorAll(`.${currentProject}`).forEach(toDoDOM => {
---
+++ src/submitToDoForm.js
@@ -0,0 +1,25 @@
+import { toDo } from './toDo.js';
+import { createToDoDOM } from './createToDoDOM';
+
+function submitToDoForm() {
+    let currentProject = localStorage.getItem('currentProject');
+    let projects = JSON.parse(localStorage.getItem('projects'));
+
+    window.formSubmit = function formSubmit() {
+        const title = document.querySelector('#form-title').value;
+        const description = document.querySelector('#form-description').value;
+        const date = document.querySelector('#form-date').value;
+        const priority = document.querySelector('input[name="form-priority"]:checked').value;
+        document.querySelector('#to-do-form').reset();
+        document.querySelector('#to-do-form').style.display = 'none';
+
+        let aToDo = toDo(title, description, date, priority);
+        createToDoDOM(aToDo, currentProject);
+        projects[currentProject].push(aToDo);
+        localStorage.setItem('projects', JSON.stringify(projects));
+    }
+
+    document.querySelector('#to-do-form').setAttribute('onsubmit', 'formSubmit();return false');
+}
+
+export { submitToDoForm }
